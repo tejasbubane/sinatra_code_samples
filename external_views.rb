27330a79e68702e8b24882c33b18089b4eed58d5
Tejas Bubane <tejas.bubane@infibeam.net>
2015-02-23 16:05:49 +0530

    Before and After filters.

--- external_views.rb
+++ external_views.rb
@@ -2,8 +2,20 @@
 
 require 'sinatra'
 
+before do
+  @website_name = 'Sinatra demo app'
+end
+
+before('/index/?') do
+  @action_name = 'Index'
+end
+
 get '/index/?' do
   @name = 'Tejas Bubane'
   @email = 'tejasbubane@gmail.com'
   erb :index
+end
+
+after do
+  puts 'Some task after request is served.'
 end
