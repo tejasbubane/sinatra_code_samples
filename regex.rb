e2590a4e4785a2a67fdebf53263240979a9695fb
Tejas Bubane <tejas.bubane@infibeam.net>
2015-02-23 14:54:43 +0530

    Routes regex and priority.

---
+++ regex.rb
@@ -0,0 +1,19 @@
+# Example with regular expressions in routes
+# Also shows that the first sufficient match is taken even though there exist
+# more accurate matches later
+
+require 'sinatra'
+
+get %r{/(sp|gr)eedy} do
+  "This is speedy and greedy"
+end
+
+get '/speedy' do
+  "This is only speedy"
+end
+
+get '/greedy' do
+  "This is only greedy"
+end
+
+# Only the first URL will be matched
