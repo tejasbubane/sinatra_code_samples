7f4cf70b2062a9ed92352f5e1d8c3cbb790671f6
Tejas Bubane <tejas.bubane@infibeam.net>
2015-02-25 15:56:47 +0530

    Inspecting routes.

---
+++ route_inspector.rb
@@ -0,0 +1,24 @@
+# Calling other routes from a route
+
+require 'sinatra'
+
+get '/example' do
+  'go to /inspect/example'
+end
+
+get '/inspect/*' do
+  route = '/' + params[:splat].first
+  data = call env.merge('PATH_INFO' => route)
+  result = "Status: #{data[0]}\n"
+
+  data[1].each do |header, value|
+    result << "#{header}: #{value}\n"
+  end
+
+  data[2].each do |body_line|
+    result << body_line
+  end
+
+  content_type :txt
+  result
+end
